ed4ff2766b771bdcd26597af9418f4ad6da2e4a9
Benjamin Taylor <staylor.ben@gmail.com>
2018-05-28 11:58:50 -0700

    added basic login functionality

--- server.js
+++ server.js
@@ -7,7 +7,28 @@ const superagent = require('superagent');
 
 const app = express();
 const PORT = process.env.PORT || 3000;
-const constring = 'postgres://localhost:5432';
+// const constring = 'postgres://localhost:5432';
+//BEN'S CONSTRING
+const constring = 'postgres://benjamin:postgrespassword@localhost:5432/badmovietonight';
 const client = new pg.Client(process.env.DATABASE_URL || constring);
+client.connect();
+client.on('error', err => console.log(err));
+
+app.use(cors());
+app.use(express.urlencoded());
+app.use(express.json());
+app.use(express.static('/'));
+
+app.get('/login/:username', (req,res) => {
+  let SQL = 'SELECT username, password, preferences FROM users WHERE username = $1;';
+  let values = [req.params.username];
+  client.query(SQL, values)
+    .then(result => res.send(result.rows[0]))
+    .catch(console.error);
+});
+
+app.get('*', (req, res) => res.status(403).send('This route does not exist'));
+
+
 
 app.listen(PORT, () => console.log(`Listening on port: ${PORT}`));
