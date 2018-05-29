bb86e351c3c1531eb9baa649263538825b6e9677
Jenny Lawrence <jlawrence010@gmail.com>
2018-05-29 16:54:43 -0700

    added page buttons and fixed people styling

--- server.js
+++ server.js
@@ -59,6 +59,25 @@ app.get('/bmt/movies', (req, res) => {
     });
 });
 
+app.get('/bmt/person', (req, res) => {
+  console.log('on server /bmt/person');
+  superagent.get(`${TMDB_API_URL}/search/person`)
+    .query({
+      api_key: process.env.TMDB_TOKEN,
+      query: req.query.searchFor,
+      page: req.query.page,
+      language: 'en-US',
+      adult: false,
+    })
+    .then(response => {
+      console.log('in superagent');
+      res.send(response.body);
+    })
+    .catch((err) => {
+      console.log(err);
+    });
+});
+
 app.get('/bmt/search', (req, res) => {
   console.log('on server for bmt/search');
   console.log('req.query',req.query);
